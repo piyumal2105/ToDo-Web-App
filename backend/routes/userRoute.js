95d951e33765e73a1645ef2ddb33c7870122d25f
Piyumal Madhuwantha <121705632+piyumal2105@users.noreply.github.com>
2024-11-25 21:41:17 +0530

    fix issues

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { registerUser, loginUser } from "../controllers/useController.js";
+import { registerUser, loginUser } from "../controllers/userController.js";
 
 const router = express.Router();
 
